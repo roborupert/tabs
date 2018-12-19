e729e50e97cb56dface5330cddbe38fd6fb98b9b
Boris Levitan <mmbug256@gmail.com>
2018-12-19 20:06:17 +0200

    ./upload.sh

--- jobs/setwebhook.rb
+++ jobs/setwebhook.rb
@@ -1,14 +1,13 @@
 require_relative './requires'
 logger = CronLogger.new
 
-hook = 'https://tabik.herokuapp.com/hook/'
+hook = 'https://dksd.herokuapp.com/hook/'
 puts "Bot: " + ARGV[0]
 puts "Setting webhook ... "
 b = Bot.find(tele: ARGV[0])
-puts b.inspect
 puts "Token: #{b.token}"
 url = hook + b.token.to_s
 puts "Webhook: #{url}"
 from_bot = Telegram::Bot::Api.new(b.token)
+from_bot.setWebhook(url: url)
 puts from_bot.getWebhookInfo.inspect
-from_bot.setWebhook(url: ARGV[1])
