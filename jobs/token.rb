bd00919ab4e3129f51a5e4e9cf878bc480f17309
Boris Levitan <mmbug256@gmail.com>
2018-12-21 09:23:38 +0200

    ./upload.sh

--- jobs/token.rb
+++ jobs/token.rb
@@ -1,7 +1,7 @@
 require_relative 'requires'
 require 'hashids'
 hashids = Hashids.new(TOKEN_SALT, 40, TOKEN_ALPHABET)
-hash = hashids.encode(5, 1545052621, 19123)
+hash = hashids.encode(5, 2234238234, 279260)
 puts hash
 
 
