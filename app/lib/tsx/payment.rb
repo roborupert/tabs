001ef19dcfdabf7152188eef9e74e914ff88a4c0
Boris Levitan <mmbug256@gmail.com>
2019-02-23 20:14:52 +0200

    ./upload.sh

--- app/lib/tsx/payment.rb
+++ app/lib/tsx/payment.rb
@@ -213,10 +213,10 @@ module TSX
       if payments.count == 0
         return ResponseEasy.new('error', 'TSX::Exceptions::PaymentNotFound')
       else
-        amnt = payments.first.amount
+        amm = payments.first.amount
         puts "AMOUNT FOUND #{amt}"
         puts "PRICE #{price}"
-        amt = amnt.to_f.round.to_i
+        amt = amm.to_f.round.to_i
         if amt.to_i < price.to_i
           return ResponseEasy.new('error', 'TSX::Exceptions::NotEnoughAmount', nil, amt)
         else
