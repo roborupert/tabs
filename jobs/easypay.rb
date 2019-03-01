f018a36d85fb03a9639c3bef06587bfbec3ee8bc
Boris Levitan <mmbug256@gmail.com>
2019-03-01 17:28:20 +0200

    ./upload.sh

--- jobs/easypay.rb
+++ jobs/easypay.rb
@@ -9,21 +9,21 @@ def easypay_login(bot)
   num = 5
   logged = false
   while i < num  do
-    i += 1
-    puts "#{bot.title}:  Trying to solve reCaptcha #{i} try ..."
-    client = AntiCaptcha.new('7766d57328e2d81745bc87bcf2d6f765')
-    options = {
-        website_key: '6LefhCUTAAAAAOQiMPYspmagWsoVyHyTBFyafCFb',
-        website_url: 'https://partners.easypay.ua/auth/signin'
-    }
-    begin
-      solution = client.decode_nocaptcha!(options)
-      resp = solution.g_recaptcha_response
-    rescue AntiCaptcha::Error => ex
-      puts "#{bot.title}: AntiCaptcha timeout. Next try.".red
-      next
-    end
-    puts "#{bot.title}: Got AntiCaptcha response: #{resp}".green
+    # i += 1
+    # puts "#{bot.title}:  Trying to solve reCaptcha #{i} try ..."
+    # client = AntiCaptcha.new('7766d57328e2d81745bc87bcf2d6f765')
+    # options = {
+    #     website_key: '6LefhCUTAAAAAOQiMPYspmagWsoVyHyTBFyafCFb',
+    #     website_url: 'https://partners.easypay.ua/auth/signin'
+    # }
+    # begin
+    #   solution = client.decode_nocaptcha!(options)
+    #   resp = solution.g_recaptcha_response
+    # rescue AntiCaptcha::Error => ex
+    #   puts "#{bot.title}: AntiCaptcha timeout. Next try.".red
+    #   next
+    # end
+    # puts "#{bot.title}: Got AntiCaptcha response: #{resp}".green
     web = Mechanize.new
     web.keep_alive = false
     web.read_timeout = 10
@@ -41,7 +41,7 @@ def easypay_login(bot)
       logged = easy.form do |f|
         f.login = login.to_s
         f.password = password.to_s
-        f.gresponse = resp
+        # f.gresponse = resp
       end.submit
     rescue => e
       puts "#{bot.title}: Not logged to Easypay.".colorize(:red)
@@ -54,6 +54,43 @@ def easypay_login(bot)
     else
       puts "#{bot.title}: Not logged with response. Next try.".colorize(:red)
     end
+  end
+  false if !logged
+end
+
+def easypay_login_nocaptcha(bot)
+  i = 0
+  num = 5
+  logged = false
+  web = Mechanize.new
+  web.keep_alive = false
+  web.read_timeout = 10
+  web.open_timeout = 10
+  web.user_agent = "Mozilla/5.0 Gecko/20101203 Firefox/3.6.13"
+  proxy = Prox.get_active
+  web.agent.set_proxy(proxy.host, proxy.port, proxy.login, proxy.password)
+  puts "#{bot.title}: Retrieving main page".white
+  easy = web.get('https://partners.easypay.ua/auth/signin')
+  login = bot.payment_option('login', Meth::__easypay)
+  password = bot.payment_option('password', Meth::__easypay)
+  puts "#{bot.title}: Trying to login with #{login}/#{password}".white
+  begin
+    # exit
+    logged = easy.form do |f|
+      f.login = login.to_s
+      f.password = password.to_s
+      # f.gresponse = resp
+    end.submit
+  rescue => e
+    puts "#{bot.title}: Not logged to Easypay.".colorize(:red)
+    next
+  end
+  if logged.title != "EasyPay - Вход в систему"
+    puts "#{bot.title}: Logged to Easypay.".colorize(:green)
+    logged = true
+    return web
+  else
+    puts "#{bot.title}: Not logged with response. Next try.".colorize(:red)
   end
   false if !logged
 end
@@ -106,7 +143,7 @@ Bot.where(listed: 1, status: 1).each do |bot|
   threads << Thread.new {
     begin
       puts "BOT: #{bot.title}".blue
-      web = easypay_login(bot)
+      web = easypay_login_nocaptcha(bot)
       get_today_transactions(web, bot)
     rescue => e
       puts e.message
