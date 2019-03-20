3d42ca681868e5f40ce343ab983b519f1c999ba3
The Autobot <dksd101@protonmail.com>
2019-03-20 09:24:40 +0300

    ./upload.sh

--- app/controllers/BotController.rb
+++ app/controllers/BotController.rb
@@ -23,6 +23,8 @@ class BotController < TSX::ApplicationController
   end
 
   post '/hook/*' do
+	[200, {}, ["----------------------- SUCCESS"]]
+	return	  
     begin
       mess = ''
       token = params[:splat].first
