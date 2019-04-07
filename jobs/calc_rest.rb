6860cf3f6903e5e4de60f906af760a42cb07f11a
DKSD 101 <dksd101@protonmail.com>
2019-04-07 13:32:37 -0400

    ./upload.sh

--- jobs/calc_rest.rb
+++ jobs/calc_rest.rb
@@ -2,11 +2,8 @@ require_relative './requires'
 logger = CronLogger.new
 # DB.logger = logger
 
-# logger.noise "Deleting all stats for all bots"
-# Stat.where('bot in [1..800]').delete
-
-logger.noise "Calculating data for charts ... "
-m_int = [*Date.parse('2019-02-01') .. Date.today]
+logger.noise "Deleting all rest for all bots"
+Rest.where('bot in [1..800]').delete
 
 threads = []
 Bot.where(listed: 1, status: 1).each do |bot|
@@ -24,7 +21,7 @@ Bot.where(listed: 1, status: 1).each do |bot|
         products.each do |product|
           count_active = Item.where(product: product[:entity_id], district: district.id, status: Item::ACTIVE, bot: bot.id).count
           logger.noise "      #{product[:entity_russian]} count: #{count_active}"
-          Rest.create(bot: bot.id, product: product.id, district: district.id, items: count_active)
+          Rest.create(bot: bot.id, product: product[:entity_id], district: district.id, items: count_active)
         end
       end
     end
