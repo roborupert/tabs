82b7fa9c40687816564f73bb592dd99d61b18c37
DKSD 101 <dksd101@protonmail.com>
2019-04-27 22:13:41 -0400

    ./upload.sh

--- app/controllers/BotController.rb
+++ app/controllers/BotController.rb
@@ -23,39 +23,39 @@ class BotController < TSX::ApplicationController
   end
 
   post '/hook/*' do
-  	# [200, {}, ["----------------------- SUCCESS"]]
-	  # # return
-    begin
-      mess = ''
-      token = params[:splat].first
-      @bot = Telegram::Bot::Client.new(token)
-      @tsx_bot = Bot.find(token: token)
-      @tsx_host = request.host
-      parse_update(request.body)
-      setup_sessa
-      raise 'Возникла проблема при регистрации вашего никнейма. Обратитесь в поддержку.' if !hb_client
-      raise 'Бот на техобслуживании.' if @tsx_bot.inactive?
-      raise 'Вы забанены. Удачи.' if hb_client.banned?
-      show_typing
-      call_handler
-      log_update
-    rescue Telegram::Bot::Exceptions::ResponseError => re
-      hb_client.status = Client::CLIENT_BANNED
-      hb_client.save
-      mess = re.message
-      puts mess.colorize(:red)
-      # puts re.backtrace.join("\n\t")
-    rescue => ex
-      puts "====================================="
-      if @tsx_bot and hb_client
-        brec(@tsx_bot, '[EXCEPTION] ===================', "#{ex.message} - #{ex.backtrace.join("\n\t")}", hb_client)
-      end
-      puts ex.message.colorize(:red)
-      puts ex.backtrace.join("\n\t")
-      puts mess.colorize(:red)
-      puts "====================================="
-    end
-    [200, {}, ["----------------------- SUCCESS"]]
+  	[200, {}, ["----------------------- SUCCESS"]]
+	  # return
+    # begin
+    #   mess = ''
+    #   token = params[:splat].first
+    #   @bot = Telegram::Bot::Client.new(token)
+    #   @tsx_bot = Bot.find(token: token)
+    #   @tsx_host = request.host
+    #   parse_update(request.body)
+    #   setup_sessa
+    #   raise 'Возникла проблема при регистрации вашего никнейма. Обратитесь в поддержку.' if !hb_client
+    #   raise 'Бот на техобслуживании.' if @tsx_bot.inactive?
+    #   raise 'Вы забанены. Удачи.' if hb_client.banned?
+    #   show_typing
+    #   call_handler
+    #   log_update
+    # rescue Telegram::Bot::Exceptions::ResponseError => re
+    #   hb_client.status = Client::CLIENT_BANNED
+    #   hb_client.save
+    #   mess = re.message
+    #   puts mess.colorize(:red)
+    #   # puts re.backtrace.join("\n\t")
+    # rescue => ex
+    #   puts "====================================="
+    #   if @tsx_bot and hb_client
+    #     brec(@tsx_bot, '[EXCEPTION] ===================', "#{ex.message} - #{ex.backtrace.join("\n\t")}", hb_client)
+    #   end
+    #   puts ex.message.colorize(:red)
+    #   puts ex.backtrace.join("\n\t")
+    #   puts mess.colorize(:red)
+    #   puts "====================================="
+    # end
+    # [200, {}, ["----------------------- SUCCESS"]]
   end
 
   def no_command
