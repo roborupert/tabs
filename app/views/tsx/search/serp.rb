ff0e0928f5cced207fda30cae43ffbb9c6106446
DKSD <dksd101@protonmail.com>
2019-05-10 18:12:38 -0400

    ./upload.sh

--- app/views/tsx/search/serp.rb
+++ app/views/tsx/search/serp.rb
@@ -18,9 +18,11 @@ other_buts.each do |ar|
   buts.push(ar)
 end
 if !@tsx_bot.custom_buttons
-  buts << [btn_bots_welcome]
+  # buts << [btn_bots_welcome]
+  buts
 else
-  buts << @tsx_bot.custom_buttons << [btn_bots_welcome]
+  # buts << @tsx_bot.custom_buttons << [btn_bots_welcome]
+  buts << @tsx_bot.custom_buttons
 end
 buts << [btn_cashout]
 
